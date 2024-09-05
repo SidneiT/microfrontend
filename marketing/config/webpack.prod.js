f4debd3e112e7ce26ca5b02fb781c447e07d86f2
Sidnei Teixeira <sidnei.teixeira@outlook.com>
2024-09-04 23:43:53 -0300

    chore: add prod webpack config

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -4,23 +4,22 @@ const packageJson = require('../package.json')
 
 const commonConfig = require('./webpack.common')
 
-const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: 'index.html'
-    }
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
       },
       shared: packageJson.dependencies
-    })
+    }),
   ]
 }
 
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge(commonConfig, prodConfig)
