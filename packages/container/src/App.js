a41374635bf3f4cc307da6002fbd8f7eaa4e8812
Sidnei Teixeira <sidnei.teixeira@outlook.com>
2024-09-25 00:33:11 -0300

    feat: add dashboard page

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,25 +1,33 @@
-import {
-  createGenerateClassName,
-  StylesProvider,
-} from "@material-ui/core/styles";
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
+import { createBrowserHistory } from 'history';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
+import { Redirect, Route, Switch, Router } from 'react-router-dom';
 
-import Header from "./components/Header";
-import Progress from "./components/Progress";
+import Header from './components/Header';
+import Progress from './components/Progress';
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
+
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory()
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard')
+    }
+  }, [isSignedIn])
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
@@ -28,6 +36,10 @@ export default () => {
               <Route path="/auth" >
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path="/" >
                 <MarketingLazy onSignedIn={() => setIsSignedIn(true)} />
               </Route>
@@ -35,6 +47,6 @@ export default () => {
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
